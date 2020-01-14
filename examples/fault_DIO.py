edec01646c7ceb688c32fa7433722e47af878dad
lawsonkeith <keithl.online@gmail.com>
2020-01-14 09:52:21 +0000

    closed down button correctly in fault example

--- examples/fault_DIO.py
+++ examples/fault_DIO.py
@@ -12,12 +12,16 @@ from gpiozero import PWMLED
 from gpiozero import LED 	# in GPIOZero outputs are called LEDs???
 from time import sleep
 from PiIO import PiIO_DIO12_Mapper
+from PiIO import PiIO_col
+import sys
 
 # @@@@ Example code here @@@@
 #
 # attach a LED to Output 6 on the board.
 # then PWM at 10Hz, cycle duty cycle then.
 #
+
+col = PiIO_col;
 
 # @@@@ Hardware init @@@@
 #
@@ -60,60 +64,79 @@ fault = Button(io.RUN,pull_up=False);
 enable.on()
 
 #
+print (col.HOME,col.CLR,col.GREENB,col.BLACK," PiIO Example program - fault_DIO \n",col.ENDC,sep='')
 print ("1. Program to test readback of fault")
 print ("2. This is as per the basic functionality program, set an input high to enable corresponding output")
-print ("3. But if that output goes into FAULT the software can detect it so long as the solder link is made on the board")
+print ("3. But if that output goes into FAULT the software can detect it so long as the solder link LK1 is made on the board")
 print ("4. Make sure NOT to set the solder link and set the RUN pin to output as this could damage the PI when a fault occurs")
 print ("5. To test, set input 1 high then short output 1 to ground, the software should detect, then clear the fault state")
 print("6. Also note that the RUN and FAULT LED will be set permanently high now.")
 print ()
 while True:
-	if  i1.value == 1:
-		print("i1 pressed")
-		o1.on()
-	if i2.value == 1:
-		print("i2 pressed")
-		o2.on()
-	if i3.value == 1:
-		print("i3 pressed")
-		o3.on()
-	if i4.value == 1:
-		print("i4 pressed")
-		o4.on()
-	if i5.value == 1:
-		print("i5 pressed")
-		o5.on()
-	if i6.value == 1:
-		print("i6 pressed")
-		o6.value = 0.5
-	if i7.value == 1:
-		print("i7 pressed")
-		o7.on()
-	if i8.value == 1:
-		print("i8 pressed")
-		o8.on()
-	if i9.value == 1:
-		print("i9 pressed")
-		o9.on()
-	if i10.value == 1:
-		print("i10 pressed")
-		o10.on()
-	if i11.value == 1:
-		print("i11 pressed")
-		o11.on()
-	if i12.value == 1:
-		print("i12 pressed")
-		o12.on()
+	try:
+		if  i1.value == 1:
+			print("i1 pressed")
+			o1.on()
+		if i2.value == 1:
+			print("i2 pressed")
+			o2.on()
+		if i3.value == 1:
+			print("i3 pressed")
+			o3.on()
+		if i4.value == 1:
+			print("i4 pressed")
+			o4.on()
+		if i5.value == 1:
+			print("i5 pressed")
+			o5.on()
+		if i6.value == 1:
+			print("i6 pressed")
+			o6.value = 0.5
+		if i7.value == 1:
+			print("i7 pressed")
+			o7.on()
+		if i8.value == 1:
+			print("i8 pressed")
+			o8.on()
+		if i9.value == 1:
+			print("i9 pressed")
+			o9.on()
+		if i10.value == 1:
+			print("i10 pressed")
+			o10.on()
+		if i11.value == 1:
+			print("i11 pressed")
+			o11.on()
+		if i12.value == 1:
+			print("i12 pressed")
+			o12.on()
 
-	if fault.value == 0:
-		print("SHORT CCT FAULT DETECTED!")
-		print("RESET FAULT...")
-		enable.off()
-		sleep(5)
-		enable.on()
-		print("RESET COMPLETE")
+		if fault.value == 0:
+			print("SHORT CCT FAULT DETECTED!")
+			print("RESET FAULT...")
+			enable.off()
+			sleep(5)
+			enable.on()
+			print("RESET COMPLETE")
 
-	sleep(1)
+		sleep(1)
+
+	except KeyboardInterrupt:
+		# required to close down program correctly in case of ctrl+c exit
+		print("bye")
+		i1.close()
+		i2.close()
+		i3.close()
+		i4.close()
+		i5.close()
+		i6.close()
+		i7.close()
+		i8.close()
+		i9.close()
+		i10.close()
+		i11.close()
+		i12.close()
+		sys.exit()
 
 
 
